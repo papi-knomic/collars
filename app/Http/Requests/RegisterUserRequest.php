841df85e644d34754fc47c527bc9ddd61382e6fa
Samson Moses <knomic10@gmail.com>
2022-11-29 11:31:12 +0100

    first commit

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+            'role' => 'required|string',
+            'location' => 'required|string'
+        ];
+    }
+}
