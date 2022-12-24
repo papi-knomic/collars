91cd4a99de07f7bba443aaf443ea5bb29e85a1b6
Samson Moses <knomic10@gmail.com>
2022-12-24 12:12:31 +0100

    Added scope and relations to Job and User

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -14,5 +15,18 @@ class Job extends Model
     ];
 
     protected $guarded = ['id'];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id', 'user_id');
+    }
+
+    /**
+     * @var mixed
+     */
+    public function scopeIsActive($query, int $arg)
+    {
+        return $query->where('is_active', $arg);
+    }
 
 }
